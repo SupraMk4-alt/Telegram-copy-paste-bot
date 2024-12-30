7f437835e5537ff600ad23045a558f40e6045fc3
chutphad <supra72625@gmail.com>
2024-12-30 18:52:09 +0530

    Update start.py

--- start.py
+++ start.py
@@ -3,7 +3,6 @@ import os
 import time
 
 def main():
-    os.system('chcp 65001 > nul')  # Set the code page to UTF-8 (Windows-specific)
     print("Running the script...")    # Print message
     time.sleep(3)                 # Wait for 3 seconds
     os.system('python main.py')   # Run the Python script
